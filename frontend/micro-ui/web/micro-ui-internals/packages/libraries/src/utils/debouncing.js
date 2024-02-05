bc58722f17019181906baefeb6d540af62089821
Tulika-eGov <74046137+Tulika-eGov@users.noreply.github.com>
2024-02-05 17:59:17 +0530

    PFM-5477 : Update debouncing.js
    Duplicate issue fix

--- frontend/micro-ui/web/micro-ui-internals/packages/libraries/src/utils/debouncing.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/libraries/src/utils/debouncing.js
@@ -1,5 +1,24 @@
 import debounce from 'lodash/debounce';
 
 export const debouncing = (func, delay) => {
-    return debounce(func,delay);
+    // return debounce(func,delay);
+    let timeout;
+//Added this custom debouncing so that at first trigger its executing immediately and subsequent triggers it gives the delay
+  return function() {
+    const context = this;
+    const args = arguments;
+
+    // If there's an existing timeout, clear it
+    if (timeout) {
+      clearTimeout(timeout);
+    } else {
+      // If no timeout exists, execute the function immediately on the first click
+      func.apply(context, args);
+    }
+
+    // Set a new timeout for subsequent clicks
+    timeout = setTimeout(function() {
+      timeout = null; // Reset the timeout variable
+    }, delay);
+  };
 }
