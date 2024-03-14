ce0e26f0c56a3f5ed3c7a366bb461b2c292ac0f3
pitabash-eGov <pitabash.choudhdry@egovernments.org>
2024-03-14 11:06:55 +0530

    bug fix

--- frontend/works_shg_app/lib/pages/employee/mb_filter_page.dart
+++ frontend/works_shg_app/lib/pages/employee/mb_filter_page.dart
@@ -1,6 +1,7 @@
 import 'package:digit_components/digit_components.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:works_shg_app/blocs/employee/mb/measurement_book.dart';
 import 'package:works_shg_app/router/app_router.dart';
 
 import '../../blocs/wage_seeker_registration/wage_seeker_location_bloc.dart';
@@ -19,109 +20,118 @@ class MBFilterPage extends StatefulWidget {
 class _MBFilterPageState extends State<MBFilterPage> {
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<WageSeekerLocationBloc, WageSeekerLocationState>(
+    return BlocBuilder<MeasurementInboxBloc, MeasurementInboxState>(
       builder: (context, state) {
-        return state.maybeWhen(
-          orElse: () {
-            return const SizedBox.shrink();
-          },
-          loaded: (location) {
-            return Scaffold(
-              appBar: AppBar(
-                titleSpacing: 0,
-                title: const AppBarLogo(),
-              ),
-              drawer: DrawerWrapper(
-                Drawer(
-                  child: SideBar(
-                    module: CommonMethods.getLocaleModules(),
+        return state.maybeMap(orElse: () => const SizedBox.shrink(),
+        loaded: (value) {
+           return BlocBuilder<WageSeekerLocationBloc, WageSeekerLocationState>(
+          builder: (context, state) {
+            return state.maybeWhen(
+              orElse: () {
+                return const SizedBox.shrink();
+              },
+              loaded: (location) {
+                return Scaffold(
+                  appBar: AppBar(
+                    titleSpacing: 0,
+                    title: const AppBarLogo(),
                   ),
-                ),
-              ),
-              body: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: ScrollableContent(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  footer: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Expanded(
-                          flex: 2,
-                          child: DigitOutLineButton(
-                            label: "Clear",
-                            onPressed: () {},
-                          ),
-                        ),
-                        Expanded(
-                          flex: 2,
-                          child: DigitElevatedButton(
-                            child: const Text("Filter"),
-                            onPressed: () {},
-                          ),
-                        )
-                      ],
+                  drawer: DrawerWrapper(
+                    Drawer(
+                      child: SideBar(
+                        module: CommonMethods.getLocaleModules(),
+                      ),
                     ),
                   ),
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        IconButton(
-                            onPressed: () {
-                              context.router.pop();
-                            },
-                            icon: const Icon(Icons.close)),
-                      ],
-                    ),
-                    Row(
-                      crossAxisAlignment: CrossAxisAlignment.center,
+                  body: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ScrollableContent(
                       mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      footer: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Expanded(
+                              flex: 2,
+                              child: DigitOutLineButton(
+                                label: "Clear",
+                                onPressed: () {},
+                              ),
+                            ),
+                            Expanded(
+                              flex: 2,
+                              child: DigitElevatedButton(
+                                child: const Text("Filter"),
+                                onPressed: () {},
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
                       children: [
-                        const Icon(Icons.filter_alt),
-                        Padding(
-                          padding: const EdgeInsets.only(left: 8.0),
-                          child: Text(
-                            "Filter",
-                            style: DigitTheme
-                                .instance.mobileTheme.textTheme.headlineLarge,
-                          ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            IconButton(
+                                onPressed: () {
+                                  context.router.pop();
+                                },
+                                icon: const Icon(Icons.close)),
+                          ],
+                        ),
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            const Icon(Icons.filter_alt),
+                            Padding(
+                              padding: const EdgeInsets.only(left: 8.0),
+                              child: Text(
+                                "Filter",
+                                style: DigitTheme.instance.mobileTheme.textTheme
+                                    .headlineLarge,
+                              ),
+                            ),
+                          ],
+                        ),
+                        const DigitTextField(label: "MB number"),
+                        const DigitTextField(label: "Project ID"),
+                        const DigitTextField(label: "Project Name"),
+                        DigitDropdown(
+                          onChanged: (value) {},
+                          value: null,
+                          label: "Ward",
+                          menuItems: location!
+                              .tenantBoundaryList!.first.boundaryList!
+                              .map((e) => e.code.toString())
+                              .toList(),
+                          // menuItems: const ["WARD1", "WARD2", "WARD3", "WARD4"],
+                          valueMapper: (value) {
+                            return value.toString();
+                          },
+                        ),
+                        DigitDropdown(
+                          onChanged: (value) {},
+                          value: null,
+                          label: "Workflow States",
+                          menuItems: value.mbInboxResponse.statusMap!.map((e) => e.state.toString()).toList(),
+                          valueMapper: (value) {
+                            return value.toString();
+                          },
                         ),
                       ],
                     ),
-                    const DigitTextField(label: "MB number"),
-                    const DigitTextField(label: "Project ID"),
-                    const DigitTextField(label: "Project Name"),
-                    DigitDropdown(
-                      onChanged: (value) {},
-                      value: null,
-                      label: "Ward",
-                      menuItems: location!
-                          .tenantBoundaryList!.first.boundaryList!
-                          .map((e) => e.code.toString())
-                          .toList(),
-                      // menuItems: const ["WARD1", "WARD2", "WARD3", "WARD4"],
-                      valueMapper: (value) {
-                        return value.toString();
-                      },
-                    ),
-                    DigitDropdown(
-                      onChanged: (value) {},
-                      value: null,
-                      label: "Workflow States",
-                      menuItems: const ["WARD1", "WARD2", "WARD3", "WARD4"],
-                      valueMapper: (value) {
-                        return value.toString();
-                      },
-                    ),
-                  ],
-                ),
-              ),
+                  ),
+                );
+              },
             );
           },
         );
+        },
+        );
+       
       },
     );
   }
