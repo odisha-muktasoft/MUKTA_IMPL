939796605fab6f2b35e444a12cdbb996f84eed70
nmohammednawaz-egov <mohammed.nawaz@egovernments.org>
2024-03-18 15:16:15 +0530

    Added code for SMS-Adapter

---
+++ reference-adapters/sms-adapter/src/main/java/org/egov/works/kafka/NotificationConsumer.java
@@ -0,0 +1,42 @@
+package org.egov.works.kafka;
+
+import org.egov.works.service.NotificationConsumerService;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Component
+@Slf4j
+public class NotificationConsumer {
+
+    @Autowired
+    private NotificationConsumerService notificationService;
+
+
+
+    /*
+     * Uncomment the below line to start consuming record from kafka.topics.consumer
+     * Value of the variable kafka.topics.consumer should be overwritten in application.properties
+     */
+    //@KafkaListener(topics = {"kafka.topics.consumer"})
+    @KafkaListener(topics = {"${contract.kafka.update.topic}",
+                             "${estimate.kafka.update.topic}",
+                             "${expense.billing.bill.create}",
+                             "${expense.billing.bill.update}",
+                             "${musterroll.kafka.update.topic}"})
+    public void listen(final HashMap<String, Object> record,@Header(KafkaHeaders.RECEIVED_TOPIC) String topic) {
+
+        notificationService.fetchServiceBasedOnTopic(record,topic);
+
+
+        //TODO
+
+    }
+}
