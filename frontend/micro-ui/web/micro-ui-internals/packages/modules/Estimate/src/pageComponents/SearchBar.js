0153d5db0f43378875c479c3ad19c460407a445d
Tulika-eGov <tulika.kundu@egovernments.org>
2024-04-01 09:49:22 +0530

    [PFM-5550,PFM-5948,PFM-5956] : updated the code to solve the state issue and search bar alignment issue

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/Estimate/src/pageComponents/SearchBar.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/Estimate/src/pageComponents/SearchBar.js
@@ -76,7 +76,7 @@ const SearchBar = (props) => {
   };
 
   return (
-    <div className={"search-bar-sor"}>
+    <div className={"search-bar-sor"} style={{marginLeft:"1.4rem"}}>
       <TextInput type="text" name={"Search"} placeholder={t("SEARCH_SOR_HINT")} value={inputValue} onChange={handleInputChange} customClass="search-sor-input"/>
       {suggestions?.length > 0 && (
       <ul
