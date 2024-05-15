07ddeea32764d055619d4f0c5f75060ef7b8ded1
Lokendra Tyagi <lokendra.tyagi@egovernments.org>
2024-05-15 15:20:25 +0530

    added authorisation for elasticsearch

--- reference-adapters/ifms-adapter/src/main/java/org/egov/utils/ESLogUtils.java
+++ reference-adapters/ifms-adapter/src/main/java/org/egov/utils/ESLogUtils.java
@@ -19,6 +19,8 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 
 import javax.validation.Valid;
 import java.util.Collections;
@@ -44,6 +46,9 @@ public class ESLogUtils {
 
     @Autowired
     private IfmsAdapterProducer ifmsAdapterProducer;
+
+    @Autowired
+    private ESAuthUtil esAuthUtil;
 
     public @Valid JITRequestLog saveAuthenticateRequest(String request, String authResponse) {
         JITRequestLog jitRequestLog = null;
@@ -199,11 +204,16 @@ public class ESLogUtils {
 
         Object response = new HashMap<>();
         try {
-            response = restTemplate.postForObject(uri.toString(), request, Map.class);
-            log.info("Elasticsearch query executed." + response);
+            final HttpHeaders headers = new HttpHeaders();
+            headers.add("Authorization", esAuthUtil.getESEncodedCredentials());
+            final HttpEntity<Object> entity = new HttpEntity<>(request, headers);
+            log.info("Request: " + objectMapper.writeValueAsString(request));
+            log.info("Entity: " + objectMapper.writeValueAsString(entity));
+            response = restTemplate.postForObject(uri.toString(), entity, Map.class);
         } catch (Exception e) {
             log.error("Exception occurred while executing query in indexer : ", e);
-            throw e;
+            throw new RuntimeException("Exception occurred while executing query in indexer", e);
+//            throw e;
         }
         return response;
     }
