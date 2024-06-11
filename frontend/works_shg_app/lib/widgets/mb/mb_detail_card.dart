11f55505203497be5dda08afc4bfb91f9c915edb
pitabash-eGov <pitabash.choudhdry@egovernments.org>
2024-06-11 16:18:07 +0530

    padding issue decreased in primary details

--- frontend/works_shg_app/lib/widgets/mb/mb_detail_card.dart
+++ frontend/works_shg_app/lib/widgets/mb/mb_detail_card.dart
@@ -25,63 +25,67 @@ class CommonMBCard extends StatelessWidget {
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
      var t = AppLocalizations.of(context);
-    return DigitCard(
-      child: Column(
-        crossAxisAlignment: widget != null
-            ? CrossAxisAlignment.start
-            : CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          headLabel != null ? Text(headLabel!,
-          style: DigitTheme.instance.mobileTheme.textTheme.headlineMedium,
-          ) : const SizedBox.shrink(),
-          TableFormCard(
-            element: items,
-          ),
-
-
-          show
-              ? Container(
-                  padding: const EdgeInsets.only(bottom: 6),
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      SizedBox(
-                        width: MediaQuery.of(context).size.width / 2,
-                        child: Text(
-                          t.translate(i18.measurementBook.mbSlaDaysRemaining),
-                          style: theme.textTheme.headline5,
-                          textAlign: TextAlign.start,
-                        ),
-                      ),
-                      const SizedBox(width: 0),
-                      Flexible(
-                          child: Padding(
-                        padding: const EdgeInsets.only(top: 1.4),
-                        child: Container(
-                          padding: const EdgeInsets.only(
-                              left: 12, right: 12, top: 4, bottom: 4),
-                          color: sla! < 0
-                              ? Colors.red.shade100
-                              : const DigitColors().paleLeafGreen,
+    return Card(
+      
+      child: Padding(
+        padding: const EdgeInsets.only(left:8.0,right: 8.0, bottom: 8.0,top: 0.0),
+        child: Column(
+          crossAxisAlignment: widget != null
+              ? CrossAxisAlignment.start
+              : CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            headLabel != null ? Text(headLabel!,
+            style: DigitTheme.instance.mobileTheme.textTheme.headlineMedium,
+            ) : const SizedBox.shrink(),
+            TableFormCard(
+              element: items,
+            ),
+      
+      
+            show
+                ? Container(
+                    padding: const EdgeInsets.only(bottom: 6),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(
+                          width: MediaQuery.of(context).size.width / 2,
                           child: Text(
-                            sla.toString(),
-                            style: DigitTheme
-                                .instance.mobileTheme.textTheme.bodySmall!
-                                .copyWith(
-                              color: sla! < 0
-                                  ? const DigitColors().lavaRed
-                                  : const DigitColors().darkSpringGreen,
-                            ),
+                            t.translate(i18.measurementBook.mbSlaDaysRemaining),
+                            style: theme.textTheme.headline5,
+                            textAlign: TextAlign.start,
                           ),
                         ),
-                      )),
-                    ],
-                  ),
-                )
-              : const SizedBox.shrink(),
-          widget ?? const SizedBox.shrink()
-        ],
+                        const SizedBox(width: 0),
+                        Flexible(
+                            child: Padding(
+                          padding: const EdgeInsets.only(top: 1.4),
+                          child: Container(
+                            padding: const EdgeInsets.only(
+                                left: 12, right: 12, top: 4, bottom: 4),
+                            color: sla! < 0
+                                ? Colors.red.shade100
+                                : const DigitColors().paleLeafGreen,
+                            child: Text(
+                              sla.toString(),
+                              style: DigitTheme
+                                  .instance.mobileTheme.textTheme.bodySmall!
+                                  .copyWith(
+                                color: sla! < 0
+                                    ? const DigitColors().lavaRed
+                                    : const DigitColors().darkSpringGreen,
+                              ),
+                            ),
+                          ),
+                        )),
+                      ],
+                    ),
+                  )
+                : const SizedBox.shrink(),
+            widget ?? const SizedBox.shrink()
+          ],
+        ),
       ),
     );
   }
