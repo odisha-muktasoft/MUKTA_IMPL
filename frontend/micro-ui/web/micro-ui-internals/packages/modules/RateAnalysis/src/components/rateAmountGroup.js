0b5d1db08b875789d19e9396d6193926b1302829
pitabash-eGov <pitabash.choudhdry@egovernments.org>
2024-06-19 14:40:35 +0530

    changes done for handling the response of mdms-v2

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/RateAnalysis/src/components/rateAmountGroup.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/RateAnalysis/src/components/rateAmountGroup.js
@@ -13,25 +13,21 @@ const RateAmountGroup = (props) => {
     detail: { newValue, sorId },
   } = props;
 
- 
-
-    const requestCriteria = {
-      url: "/mdms-v2/v2/_search",
-      body: {
-        MdmsCriteria: {
-          tenantId: tenantId,
-          filters: 
-            {
-             sorId:sorId
-            },
-            schemaCode:"WORKS-SOR.Rates",
-            limit:100,
-            offset:0,
-          
+  const requestCriteria = {
+    url: "/mdms-v2/v2/_search",
+    body: {
+      MdmsCriteria: {
+        tenantId: tenantId,
+        filters: {
+          sorId: sorId,
         },
+        schemaCode: "WORKS-SOR.Rates",
+        limit: 100,
+        offset: 0,
       },
-      changeQueryName: "ratesQuery",
-    };
+    },
+    changeQueryName: "ratesQuery",
+  };
 
   // const requestCriteria = {
   //   url: "/mdms-v2/v2/_search",
@@ -56,9 +52,7 @@ const RateAmountGroup = (props) => {
 
   const { isLoading, data: RatesData } = Digit.Hooks.useCustomAPIHook(requestCriteria);
   useEffect(() => {
-    
-    setExistingData(RatesData?.MdmsRes?.["WORKS-SOR"]?.Rates[0]?.rate===undefined?0.0:RatesData?.MdmsRes?.["WORKS-SOR"]?.Rates[0]?.rate);
-    console.log(RatesData?.MdmsRes?.["WORKS-SOR"]?.Rates[0]?.rate,"wel");
+    setExistingData(RatesData?.mdms.length !== 0 ? (RatesData?.mdms[0].data?.rate === undefined ? 0.0 : RatesData?.mdms[0].data?.rate) : 0.0);
   }, [RatesData]);
 
   return (
