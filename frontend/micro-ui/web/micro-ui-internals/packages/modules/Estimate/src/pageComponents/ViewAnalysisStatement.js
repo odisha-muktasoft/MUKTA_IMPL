420034927b61c35c4d6ec79e64c0c5ec77ceddcc
pitabash-eGov <pitabash.choudhdry@egovernments.org>
2024-07-03 10:36:37 +0530

    console removed

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/Estimate/src/pageComponents/ViewAnalysisStatement.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/Estimate/src/pageComponents/ViewAnalysisStatement.js
@@ -157,7 +157,7 @@ const ViewAnalysisStatement = ({ formData, ...props }) => {
   }
 
   async function handleButtonClick() {
-  console.log("search")
+ 
     if (isEstimate) {
       if (formData?.SORtable?.length > 0) {
         if (isView) {
