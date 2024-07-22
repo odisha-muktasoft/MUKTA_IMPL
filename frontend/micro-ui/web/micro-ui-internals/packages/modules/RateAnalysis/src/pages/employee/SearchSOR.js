e7ca2a60ea4814e8bd54f68a85210f53f7eddaf4
Tulika-eGov <tulika.kundu@egovernments.org>
2024-07-22 11:02:31 +0530

    Updated the revise for all rates button to enable

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/RateAnalysis/src/pages/employee/SearchSOR.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/RateAnalysis/src/pages/employee/SearchSOR.js
@@ -139,8 +139,8 @@ const SearchSOR = () => {
             setPopup(true);
             setReviseAll(true);
           }}
-          //disabled={selectedSorIds?.sorType !== "W"}
-          disabled={true}
+          disabled={selectedSorIds?.sorType !== "W"}
+          //disabled={true}
         />
       </ActionBar>
       {showToast?.show && (
