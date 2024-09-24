127a8f8999622246767c5dbf5bcd129cb0d1fe7c
pitabash-eGov <pitabash.choudhdry@egovernments.org>
2024-09-24 16:06:34 +0530

    UI work in progress

--- frontend/works_shg_app/lib/widgets/atoms/app_logo.dart
+++ frontend/works_shg_app/lib/widgets/atoms/app_logo.dart
@@ -1,3 +1,4 @@
+import 'package:digit_ui_components/theme/digit_extended_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:works_shg_app/blocs/localization/app_localization.dart';
@@ -16,52 +17,55 @@ class AppLogo extends StatelessWidget {
        
           return Align(
               alignment: Alignment.centerLeft,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Padding(
-                      padding: const EdgeInsets.only(top: 0.0, bottom: 0),
-                      child: Center(
-                        child: GlobalVariables.stateInfoListModel?.logoUrl !=
-                                null
-                            ? Image(
-                                width: 130,
-                                image: NetworkImage(
-                                  GlobalVariables.stateInfoListModel!.logoUrl
-                                      .toString(),
-                                ))
-                            : const SizedBox.shrink(),
-                      )),
-                  const Padding(
-                      padding: EdgeInsets.only(left: 16),
-                      child: Text(
-                        " | ",
-                        style: TextStyle(
-                            fontSize: 19,
-                            fontWeight: FontWeight.w400,
-                            color: Color.fromRGBO(0, 0, 0, 1)),
-                      )),
-                  Align(
-                      alignment: Alignment.centerLeft,
-                      child: Padding(
-                        padding: const EdgeInsets.only(
-                          left: 8.0,
-                          right: 8.0,
-                        ),
-                        child: BlocBuilder<AppInitializationBloc,
-                            AppInitializationState>(builder: (context, state) {
-                          return Text(
-                            AppLocalizations.of(context).translate(
-                                'TENANT_TENANTS_${GlobalVariables.stateInfoListModel!.code.toString().toUpperCase()}'),
-                            style: const TextStyle(
-                                fontSize: 19,
-                                fontWeight: FontWeight.w400,
-                                color: Color.fromRGBO(0, 0, 0, 1)),
-                            textAlign: TextAlign.left,
-                          );
-                        }),
-                      ))
-                ],
+              child: Padding(
+                padding:  EdgeInsets.only(bottom:Theme.of(context).spacerTheme.spacer4),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Padding(
+                        padding: const EdgeInsets.only(top: 0.0, bottom:0),
+                        child: Center(
+                          child: GlobalVariables.stateInfoListModel?.logoUrl !=
+                                  null
+                              ? Image(
+                                  width: 130,
+                                  image: NetworkImage(
+                                    GlobalVariables.stateInfoListModel!.logoUrl
+                                        .toString(),
+                                  ))
+                              : const SizedBox.shrink(),
+                        )),
+                    const Padding(
+                        padding: EdgeInsets.only(left: 16),
+                        child: Text(
+                          " | ",
+                          style: TextStyle(
+                              fontSize: 19,
+                              fontWeight: FontWeight.w400,
+                              color: Color.fromRGBO(0, 0, 0, 1)),
+                        )),
+                    Align(
+                        alignment: Alignment.centerLeft,
+                        child: Padding(
+                          padding: const EdgeInsets.only(
+                            left: 8.0,
+                            right: 8.0,
+                          ),
+                          child: BlocBuilder<AppInitializationBloc,
+                              AppInitializationState>(builder: (context, state) {
+                            return Text(
+                              AppLocalizations.of(context).translate(
+                                  'TENANT_TENANTS_${GlobalVariables.stateInfoListModel!.code.toString().toUpperCase()}'),
+                              style: const TextStyle(
+                                  fontSize: 19,
+                                  fontWeight: FontWeight.w400,
+                                  color: Color.fromRGBO(0, 0, 0, 1)),
+                              textAlign: TextAlign.left,
+                            );
+                          }),
+                        ))
+                  ],
+                ),
               ));
        
       },
