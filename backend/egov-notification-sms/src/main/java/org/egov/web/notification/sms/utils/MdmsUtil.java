dd71f7f2dbe416bc3b4650e2ca0543edab43ae3d
manastanmay-eGov <manastanmay@egovernments.org>
2024-10-18 11:16:54 +0530

    Refactored Imports and enc-client version fixed

--- backend/egov-notification-sms/src/main/java/org/egov/web/notification/sms/utils/MdmsUtil.java
+++ backend/egov-notification-sms/src/main/java/org/egov/web/notification/sms/utils/MdmsUtil.java
@@ -4,10 +4,10 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import net.minidev.json.JSONArray;
 import org.egov.common.contract.request.RequestInfo;
+import org.egov.mdms.model.*;
 import org.egov.web.notification.sms.config.Configuration;
 
 //import org.egov.mdms.model.*;
-import org.egov.mdms.model.MdmsCriteriaReq;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
