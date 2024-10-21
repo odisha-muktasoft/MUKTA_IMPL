97a4eb1ab923f2b6350493d55d0d6cdad03e9684
kams <kamalesh.m@egovernments.org>
2024-10-21 16:01:42 +0530

    added two functions for the jobs in uiCustomizations

--- frontend/micro-ui/web/micro-ui-internals/packages/Mukta/src/configs/UICustomizations.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/Mukta/src/configs/UICustomizations.js
@@ -2882,13 +2882,91 @@ export const UICustomizations = {
   },
   ViewScheduledJobsConfig: {
     preProcess: (data) => {
-      const scheduledFrom = Digit.Utils.pt.convertDateToEpoch(data?.body?.SearchCriteria?.scheduledFrom);
-      const scheduledTo = Digit.Utils.pt.convertDateToEpoch(data.body.SearchCriteria?.scheduledTo);
+      const scheduledFrom = Digit.Utils.pt.convertDateToEpoch(data?.body?.SearchCriteria?.scheduleFrom);
+      const scheduledTo = Digit.Utils.pt.convertDateToEpoch(data.body.SearchCriteria?.scheduleTo);
       const status = data.body.SearchCriteria?.status?.code;
       data.params = { ...data.params, tenantId: Digit.ULBService.getCurrentTenantId(), includeAncestors: true };
       data.body.SearchCriteria.tenantId = Digit.ULBService.getCurrentTenantId();
       data.body.SearchCriteria = {
         ...data.body.SearchCriteria,
+        tenantId: Digit.ULBService.getCurrentTenantId(),
+        status,
+        scheduleFrom:scheduledFrom,
+        scheduleTo:scheduledTo,
+      };
+      return data;
+    },
+    customValidationCheck: (data) => {
+      //checking both to and from date are present
+      const { scheduledFrom, scheduledTo } = data;
+      if (scheduledTo !== "" && scheduledFrom === "") return { type:"warning", label: "ES_COMMON_ENTER_DATE_RANGE" };
+      else if (scheduledTo === "" && scheduledFrom !== "") return { type:"warning", label: "ES_COMMON_ENTER_DATE_RANGE" };
+
+      return false;
+    },
+    additionalCustomizations: (row, key, column, value, t, searchResult) => {
+      //here we can add multiple conditions
+      //like if a cell is link then we return link
+      //first we can identify which column it belongs to then we can return relevant result
+      switch (key) {
+        case "RA_JOB_ID":
+          return value;
+
+        case "RA_SCHEDULED_ON":
+          return Digit.DateUtils.ConvertEpochToDate(value);
+
+        case "RA_RATE_EFFECTIVE_FROM": {
+          return Digit.DateUtils.ConvertEpochToDate(value);
+        }
+
+        case "RA_NO_OF_SOR_SCHEDULED":
+          return { value };
+
+        case "RA_SUCCESSFUL": {
+          let successfulCount = 0;
+          row.sorDetails.forEach((detail) => {
+            if (detail.status === "SUCCESSFUL") {
+              successfulCount++;
+            }
+          });
+          return successfulCount;
+        }
+        case "RA_FAILED": {
+          let failedCount = 0;
+          value.forEach((detail) => {
+            if (detail.status === "FAILED") {
+              failedCount++;
+            }
+          });
+          return failedCount;
+        }
+
+        case "RA_STATUS":
+          return (
+            <div style={{ color: value === "FAILED" ? "#D4351C" : value === "COMPLETED" ? "#27AE60" : "#F47738" }}>
+              {value === "FAILED" ? "Failed" : value === "COMPLETED" ? "Completed" : value === "IN_PROGRESS" ? "In Progress" : "Scheduled"}
+            </div>
+          );
+
+        default:
+          return t("ES_COMMON_NA");
+      }
+    },
+    additionalValidations: (type, data, keys) => {
+      if (type === "date") {
+        return data[keys.start] && data[keys.end] ? () => new Date(data[keys.start]).getTime() <= new Date(data[keys.end]).getTime() : true;
+      }
+    },
+  },
+  ViewScheduledJobsExcelConfig: {
+    preProcess: (data) => {
+      const scheduledFrom = Digit.Utils.pt.convertDateToEpoch(data?.body?.reportSearchCriteria?.scheduledFrom);
+      const scheduledTo = Digit.Utils.pt.convertDateToEpoch(data.body.reportSearchCriteria?.scheduledTo);
+      const status = data.body.reportSearchCriteria?.status?.code;
+      data.params = { ...data.params, tenantId: Digit.ULBService.getCurrentTenantId(), includeAncestors: true };
+      data.body.reportSearchCriteria.tenantId = Digit.ULBService.getCurrentTenantId();
+      data.body.reportSearchCriteria = {
+        ...data.body.reportSearchCriteria,
         tenantId: Digit.ULBService.getCurrentTenantId(),
         status,
         scheduledFrom:scheduledFrom,
