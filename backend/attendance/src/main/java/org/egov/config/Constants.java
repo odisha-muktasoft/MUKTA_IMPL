12f5e06786ca1c4ac64aa67bd08ba794aa6a077d
anishEgov <anish.sharma@egovernments.org>
2025-08-28 10:58:06 +0530

    added Constants.java file to attendance

---
+++ backend/attendance/src/main/java/org/egov/config/Constants.java
@@ -0,0 +1,6 @@
+package org.egov.config;
+
+public class Constants {
+    public static final String INVALID_TENANT_ID = "INVALID_TENANT_ID";
+    public static final String INVALID_TENANT_ID_MSG = "Tenant ID is not valid";
+}
