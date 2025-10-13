24210c03152f735e8c08d5bab2a6410b44a1a0aa
SudhaKannaiah <Sudha@Kirans-MacBook-Air.local>
2025-10-13 16:12:32 +0530

    added missing files as part of central instance migration

---
+++ backend/bankaccounts/src/main/java/org/egov/kafka/Producer.java
@@ -0,0 +1,29 @@
+package org.egov.kafka;
+
+import lombok.extern.slf4j.Slf4j;
+import org.egov.common.utils.MultiStateInstanceUtil;
+import org.egov.tracer.kafka.CustomKafkaTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+// NOTE: If tracer is disabled change CustomKafkaTemplate to KafkaTemplate in autowiring
+
+@Service("bankAccountProducer")
+@Slf4j
+public class Producer {
+
+    private final CustomKafkaTemplate<String, Object> kafkaTemplate;
+
+    private final MultiStateInstanceUtil multiStateInstanceUtil;
+
+    @Autowired
+    public Producer(CustomKafkaTemplate<String, Object> kafkaTemplate, MultiStateInstanceUtil multiStateInstanceUtil) {
+        this.kafkaTemplate = kafkaTemplate;
+        this.multiStateInstanceUtil = multiStateInstanceUtil;
+    }
+
+    public void push(String tenantId, String topic , Object value){
+        String updatedTopic = multiStateInstanceUtil.getStateSpecificTopicName(tenantId,topic);
+        kafkaTemplate.send(updatedTopic,value);
+    }
+}
