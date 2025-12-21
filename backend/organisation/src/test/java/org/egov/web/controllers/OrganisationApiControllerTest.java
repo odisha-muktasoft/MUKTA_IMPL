4f8a9325a6c21ab26c52feaaf4e8cf97dd557772
Sudha-Kannaiah-eGov <sudha.kannaiah@egovernments.org>
2025-12-21 20:20:25 +0530

    Fixing failing tests

--- backend/organisation/src/test/java/org/egov/web/controllers/OrganisationApiControllerTest.java
+++ backend/organisation/src/test/java/org/egov/web/controllers/OrganisationApiControllerTest.java
@@ -17,7 +17,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.ContextConfiguration;
 import org.springframework.http.MediaType;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.web.servlet.MockMvc;
